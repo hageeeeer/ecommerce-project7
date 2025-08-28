90218da1db6aa6c571a353984bfad2370a32c755
HajarsaMer <hagersamer21@gmail.com>
2025-08-28 20:52:23 +0300

    uninstall loader

--- src/app/_components/Loading.tsx
+++ src/app/_components/Loading.tsx
@@ -1,20 +1,8 @@
-'use client'
 
 import React from 'react'
-import { Puff } from 'react-loader-spinner'
 
 export default function Loading() {
-    return (
-        <div className="flex h-[90%] justify-center items-center">
-            <Puff
-                visible={true}
-                height="100"
-                width="100"
-                color="rgb(10, 173, 10)"
-                ariaLabel="puff-loading"
-                wrapperStyle={{}}
-                wrapperClass=""
-            /></div>
-    )
-
+  return (
+    <div className="flex h-[90%] justify-center items-center">loading</div>
+  )
 }
